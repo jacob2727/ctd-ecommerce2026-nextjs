a57ef0fa6988219929c318a45b34a33af70618d6
Ismail3427 <ismailurfy@gmail.com>
2026-07-08 13:41:59 -0500

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -19,26 +19,35 @@ const CartPage = async () => {
     },
   );
 
+  console.log(cart);
+
   return (
     <main className="flex flex-col gap-5 items-center justify-center p-5">
       <h1 className="font-bold text-6xl">Cart</h1>
       <ul className="flex flex-col gap-5">
-        {cart.map((item: any) => (
-          <li
-            key={item.id}
-            className="shadow p-5 rounded-2xl hover:shadow-2xl transition-all duration-300 ease-in-out w-[50vw] flex justify-between"
-          >
-            <div className="flex flex-col gap-3">
-              <h2 className="font-bold text-2xl">{item.product.name}</h2>
-              <p>{item.product.description}</p>
-              <p>${(item.product.priceInCents / 100).toFixed(2)}</p>
-            </div>
-            <div className="flex flex-col gap-3">
-              <p>Quantity: {item.quantity}</p>
-              <QuantityChange cartId={item.id} productId={item.product.id} />
-            </div>
-          </li>
-        ))}
+        {cart.map((item: any) => {
+          item.showing && (
+            <>
+              <li
+                key={item.id}
+                className="shadow p-5 rounded-2xl hover:shadow-2xl transition-all duration-300 ease-in-out w-[50vw] flex justify-between"
+              >
+                <div className="flex flex-col gap-3">
+                  <h2 className="font-bold text-2xl">{item.product.name}</h2>
+                  <p>{item.product.description}</p>
+                  <p>${(item.product.priceInCents / 100).toFixed(2)}</p>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <p>Quantity: {item.quantity}</p>
+                  <QuantityChange
+                    cartId={item.id}
+                    productId={item.product.id}
+                  />
+                </div>
+              </li>
+            </>
+          );
+        })}
       </ul>
       <CheckoutButton cart={cart} />
     </main>
