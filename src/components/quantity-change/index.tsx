f0ddfa4d4478cf3e405046894789d1df5e6deb90
jacob2727 <71154890+jacob2727@users.noreply.github.com>
2026-07-15 13:02:28 -0500

    Update index.tsx

--- src/components/quantity-change/index.tsx
+++ src/components/quantity-change/index.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import axios from "axios";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 const QuantityChange = ({
   cartId,
@@ -13,38 +15,36 @@ const QuantityChange = ({
   cart: any;
 }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const updateQuantity = async (type: "increment" | "decrement") => {
+    try {
+      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/cart/${type}`, {
+        cartId,
+        productId,
+      });
+
+      startTransition(() => {
+        router.refresh();
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="flex flex-col gap-3">
       {cart.quantity < cart.product.stock && (
         <Button
-          onClick={async () => {
-            await axios.post(
-              `${process.env.NEXT_PUBLIC_API_URL}/cart/increment`,
-              {
-                cartId,
-                productId,
-              },
-            );
-            router.refresh();
-          }}
+          disabled={isPending}
+          onClick={() => updateQuantity("increment")}
         >
-          Add
+          {isPending ? "Updating..." : "Add"}
         </Button>
       )}
-      <Button
-        onClick={async () => {
-          await axios.post(
-            `${process.env.NEXT_PUBLIC_API_URL}/cart/decrement`,
-            {
-              cartId,
-              productId,
-            },
-          );
-          router.refresh();
-        }}
-      >
-        Subtract
+
+      <Button disabled={isPending} onClick={() => updateQuantity("decrement")}>
+        {isPending ? "Updating..." : "Subtract"}
       </Button>
     </div>
   );
